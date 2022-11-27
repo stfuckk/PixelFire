24f32971ea38f3ab490763566d22b49940b5a505
Denio1337 <danil.kolmogorov@yandex.ru>
2022-11-27 21:21:16 +0700

    Menu v2

--- test/core/src/com/pixel/fire/MenuScreen.java
+++ test/core/src/com/pixel/fire/MenuScreen.java
@@ -41,21 +41,21 @@ public class MenuScreen extends ScreenAdapter
         mainTable.setFillParent(true);
 
         stage.addActor(mainTable);
-    }
-
-    @Override
-    public void render(float delta)
-    {
-        Gdx.gl.glClearColor(.1f, .1f, .15f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.act();
-        stage.draw();
 
         addButton("Play");
         addButton("Options");
         addButton("Credits");
         addButton("Quit");
+    }
+
+    @Override
+    public void render(float delta)
+    {
+        Gdx.gl.glClearColor(.1f, .15f, .15f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
     }
 
     @Override
