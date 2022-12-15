69d66f4837e1340ec4794808deb9fd8c2d10a4d3
stfackk <denkis2205@gmail.com>
2022-12-15 20:06:55 +0700

    Merged_from_Danil

---
+++ test/core/src/com/pixel/fire/MenuScreen.java
@@ -0,0 +1,201 @@
+package com.pixel.fire;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.pixel.fire.client.Client;
+import com.pixel.fire.server.Server;
+
+import java.awt.*;
+
+public class MenuScreen extends ScreenAdapter
+{
+    private Stage stage;
+    private Viewport viewport;
+    private Skin skin;
+    private GameScreen gameScreen;
+    private Table mainTable;
+    private Table playTable;
+    private Table settingsTable;
+    private MyGame game;
+
+    public MenuScreen(AssetManager assetManager, MyGame game)
+    {
+        skin = assetManager.get(Assets.SKIN);
+
+        this.game = game;
+
+        gameScreen = new GameScreen(game, assetManager, this);
+    }
+
+    @Override
+    public void show()
+    {
+        viewport = new ExtendViewport(700,800);
+        stage = new Stage(viewport);
+
+        mainTable = new Table();
+        mainTable.setFillParent(true);
+
+        playTable = new Table();
+        playTable.setFillParent(true);
+        playTable.setVisible(false);
+
+        settingsTable = new Table();
+        settingsTable.setFillParent(true);
+        settingsTable.setVisible(false);
+
+        stage.addActor(mainTable);
+        stage.addActor(playTable);
+
+        addButton("Play", mainTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                mainTable.setVisible(false);
+                playTable.setVisible(true);
+            }
+        });
+        addButton("Options", mainTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                System.out.println("Options click");
+            }
+        });
+        addButton("Credits", mainTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                Dialog d = new Dialog("Credits", skin)
+                {
+                    {
+                        text("This beatiful game was created by these dudes:\nKolmogorov Danil Pavlovich aka Denio1337\nKozlov Denis Valeryevich aka PIDOR\nStepanyuk Dmitriy Aleksandrovich aka X3");
+                        button("OK");
+                    }
+                };
+                d.show(stage);
+                //System.out.println("Credits click");
+            }
+        });
+        addButton("Quit", mainTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                Gdx.app.exit();
+            }
+        });
+        addButton("Test", mainTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                mainTable.setVisible(false);
+                game.setScreen(gameScreen);
+            }
+        });
+        addButton("Connect", playTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                Client clientThread = new Client();
+                clientThread.StartClient();
+                try
+                {
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException e)
+                {
+                    throw new RuntimeException(e);
+                }
+                if (!clientThread.isServerStarted())
+                {
+                    Dialog d = new Dialog("Connection error", skin)
+                    {
+                        {
+                            text("Server is not created!");
+                            button("OK");
+                        }
+                    };
+                    d.show(stage);
+                }
+                else
+                {
+                    playTable.setVisible(false);
+                    game.setScreen(gameScreen);
+                }
+            }
+        });
+        addButton("Create server", playTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                    new Thread(){@Override public void run()
+                    {
+                        try
+                        {
+                            Server.start();
+                        } catch (InterruptedException e)
+                        {
+                            throw new RuntimeException(e);
+                        }
+                    }}.start();
+                    Client clientThread = new Client();
+                    clientThread.StartClient();
+                    playTable.setVisible(false);
+                    game.setScreen(gameScreen);
+            }
+        });
+        addButton("Return", playTable).addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                playTable.setVisible(false);
+                mainTable.setVisible(true);
+            }
+        });
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta)
+    {
+        Gdx.gl.glClearColor(.1f, .15f, .15f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize (int width, int height)
+    {
+        viewport.update(width, height);
+    }
+
+    public TextButton addButton(String name, Table table)
+    {
+        TextButton button = new TextButton(name, skin);
+        table.add(button).width(700).height(120).padBottom(60);
+        table.row();
+        return button;
+    }
+}
---
+++ test/core/src/com/pixel/fire/Objects/Player/Player.java
@@ -0,0 +1,197 @@
+package com.pixel.fire.Objects.Player;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+
+import static com.pixel.fire.Helper.Constants.PPM;
+
+public class Player extends GameEntity
+{
+    //
+    private static int FRAME_COLS = 8, FRAME_ROWS = 1;
+    //
+    private boolean left = false;
+    private boolean isGrounded = false;
+    private boolean isIdle = true;
+    private boolean isJumping = false;
+    private boolean isFalling = false;
+    private boolean paused = false;
+
+    private int counter = 0;
+    //
+    private TextureRegion currentFrame;
+    Animation<TextureRegion> runningAnimation;
+    Animation<TextureRegion> idleAnimation;
+    Animation<TextureRegion> jumpingAnimation;
+    float stateTime;
+    //
+    public Player(float width, float height, Body body) {
+        super(width, height, body); //super - parent class
+        this.speed = 20f;
+        Texture runSheet = new Texture("Sprites/run.png");
+        Texture jumpTexture = new Texture("Sprites/jump.png");
+        Texture idleSheet = new Texture("Sprites/idle.png");
+        //
+        TextureRegion[][] tmp;
+        //IDLE//
+        FRAME_COLS = 5;
+        tmp = TextureRegion.split(idleSheet,
+                idleSheet.getWidth() / FRAME_COLS,
+                idleSheet.getHeight() / FRAME_ROWS);
+        TextureRegion[] idleFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        idleAnimation = new Animation<TextureRegion>(0.064f, FramesCycle(tmp, idleFrames));
+
+        //JUMPING//
+        FRAME_COLS = 1;
+        tmp = TextureRegion.split(jumpTexture,
+                jumpTexture.getWidth() / FRAME_COLS,
+                jumpTexture.getHeight() / FRAME_ROWS);
+        TextureRegion[] jumpFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        jumpingAnimation = new Animation<TextureRegion>(0.064f, FramesCycle(tmp, jumpFrames));
+
+        //RUNNING//
+        FRAME_COLS = 8;
+        tmp = TextureRegion.split(runSheet,
+                runSheet.getWidth() / FRAME_COLS,
+                runSheet.getHeight() / FRAME_ROWS);
+        TextureRegion[] runFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        runningAnimation = new Animation<TextureRegion>(0.064f, FramesCycle(tmp, runFrames));
+
+        stateTime = 0f;
+        currentFrame = idleAnimation.getKeyFrame(stateTime, true);
+    }
+
+    public TextureRegion[] FramesCycle(TextureRegion[][] tmp, TextureRegion[] Frames){
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++){
+            for (int j = 0; j < FRAME_COLS; j++){
+                Frames[index++] = tmp[i][j];
+            }
+        }
+        return Frames;
+    }
+
+    @Override
+    public void update() {
+        x = body.getPosition().x * PPM;
+        y = body.getPosition().y * PPM;
+
+        checkUserInput();
+
+        //////////////GROUND CHECK///////////////
+        if
+                ((y >= 400 && y <= 400.5) && (x >= 111 && x <= 656) ||
+                (y >= 528 && y <= 528.5) && (x >= 751 && x <= 1168) ||          //1
+                (y >= 592 && y <= 592.5) && (x >= 815 && x <= 1104) ||
+                (y >= 400 && y <= 400.5) && (x >= 1263 && x <= 1808) ||
+
+                (y >= 784 && y <= 784.5) && (x >= 496 && x <= 720) ||
+                (y >= 848  && y <= 848.5) && (x >= 367 && x <= 528) ||        //2
+                (y >= 848  && y <= 848.5) && (x >= 879 && x <= 1040) ||
+                (y >= 784 && y <= 784.5) && (x >= 1199 && x <= 1423) ||
+                (y >= 848  && y <= 848.5) && (x >= 1391 && x <= 1552) ||
+
+                (y >= 976  && y <= 976.5) && (x >= 111 && x <= 272) ||
+                (y >= 976  && y <= 976.5) && (x >= 1647 && x <= 1808) ||    //3
+
+                (y >= 1104  && y <= 1104.5) && (x >= 431 && x <= 527) ||
+                (y >= 1168  && y <= 1168.5) && (x >= 495 && x <= 848) ||    //4
+                (y >= 1104  && y <= 1104.5) && (x >= 1392 && x <= 1488) ||
+                (y >= 1168  && y <= 1168.5) && (x >= 1071 && x <= 1424) )
+
+            isGrounded = true;
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        stateTime += Gdx.graphics.getDeltaTime();
+        //idle
+        if (isGrounded && isIdle && !isJumping)
+            currentFrame = idleAnimation.getKeyFrame(stateTime, true);
+        //run
+        if(!isIdle && isGrounded && body.getLinearVelocity().y == 0)
+            currentFrame = runningAnimation.getKeyFrame(stateTime, true);
+
+        //jump
+        if(isJumping || isFalling)
+            currentFrame = jumpingAnimation.getKeyFrame(stateTime, true);
+
+
+        //flip sprite
+        if(!currentFrame.isFlipX() && left)
+            currentFrame.flip(true, false);
+
+        if(currentFrame.isFlipX() && !left)
+            currentFrame.flip(true, false);
+        //
+        batch.begin();
+        batch.draw(currentFrame, (body.getPosition().x * PPM) - 64, (body.getPosition().y * PPM) - 16);
+        batch.end();
+        //
+    }
+    private void checkUserInput(){
+        velX = 0;
+
+        if(body.getLinearVelocity().y == 0 && isGrounded)
+        {
+            isJumping = false;
+            isFalling = false;
+            counter = 0;
+        }
+
+        if(isGrounded && body.getLinearVelocity().y < 0)
+            isFalling = true;
+
+        if(isGrounded && body.getLinearVelocity().x == 0)
+            isIdle = true;
+
+        if(isGrounded && body.getLinearVelocity().y > 0)
+            isGrounded = false;
+
+        if(Gdx.input.isKeyPressed(Input.Keys.D) && paused == false){
+            velX = 1;
+            left = false;
+            isIdle = false;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A) && paused == false) {
+            velX = -1;
+            left = true;
+            isIdle = false;
+        }
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.W) && counter <= 1 && paused == false)
+        {
+            float force = body.getMass() * 23;
+            body.setLinearVelocity(body.getLinearVelocity().x, 0);
+            body.applyLinearImpulse(new Vector2(0, force), body.getPosition(), true);
+            isGrounded = false;
+            isIdle = false;
+            isJumping = true;
+            counter++;
+        }
+
+        body.setLinearVelocity(velX * speed, body.getLinearVelocity().y < 25 ?
+                body.getLinearVelocity().y : 25);
+    }
+
+    public void pause(boolean state)
+    {
+        paused = state;
+    }
+
+    public void setPosition()
+    {
+        body.setTransform(10, 15, 0);
+    }
+
+    public boolean isLeft()
+    {
+        return left;
+    }
+}
